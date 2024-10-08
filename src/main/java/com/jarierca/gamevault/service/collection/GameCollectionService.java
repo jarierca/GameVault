5a3c68a79de32316120a630c877251ef3417cd91
jarierca <javialiaga2000@gmail.com>
2024-10-08 22:09:18 +0200

    Collection name now check if exist another coll with the same name

--- src/main/java/com/jarierca/gamevault/service/collection/GameCollectionService.java
+++ src/main/java/com/jarierca/gamevault/service/collection/GameCollectionService.java
@@ -1,6 +1,7 @@
 package com.jarierca.gamevault.service.collection;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.jarierca.gamevault.entity.collection.GameCollection;
 import com.jarierca.gamevault.entity.collection.Player;
@@ -19,7 +20,7 @@ public class GameCollectionService {
 
 	@Inject
 	PlayerService playerService;
-	
+
 	@Inject
 	AuthService authService;
 
@@ -29,7 +30,7 @@ public class GameCollectionService {
 
 	public List<GameCollection> findAllCollectionsByPlayerId() {
 		Long playerId = authService.getAuthenticatedUserId();
-		
+
 		return collectionRepository.findAllCollectionsByPlayerId(playerId);
 	}
 
@@ -39,15 +40,14 @@ public class GameCollectionService {
 
 	public GameCollection findByPlayerIdAndCollectionId(Long collectionId) {
 		Long playerId = authService.getAuthenticatedUserId();
-		
+
 		return collectionRepository.findByPlayerIdAndCollectionId(playerId, collectionId);
 	}
-	
+
 	public void validatePlayerCollection(Long collectionId) {
 		Long playerId = authService.getAuthenticatedUserId();
 
-		GameCollection existingCollection = collectionRepository
-				.findByPlayerIdAndCollectionId(playerId, collectionId);
+		GameCollection existingCollection = collectionRepository.findByPlayerIdAndCollectionId(playerId, collectionId);
 
 		if (existingCollection == null) {
 			throw new IllegalArgumentException("Yo can't modify this collection.");
@@ -57,8 +57,15 @@ public class GameCollectionService {
 	@Transactional
 	public GameCollection create(GameCollection collection) {
 		Long playerId = authService.getAuthenticatedUserId();
-		
+
 		Player player = playerService.findById(playerId);
+
+		Optional<GameCollection> existingCollection = collectionRepository.find("name", collection.getName())
+				.firstResultOptional();
+
+		if (existingCollection.isPresent()) {
+			throw new IllegalArgumentException("A collection with the name already exists: " + collection.getName());
+		}
 
 		collectionRepository.persist(new GameCollection(collection, player));
 		return collection;
@@ -68,7 +75,7 @@ public class GameCollectionService {
 	public GameCollection update(Long id, GameCollection updatedCollection) {
 		GameCollection existingCollection = collectionRepository.findById(id);
 		if (existingCollection != null) {
-			
+
 			existingCollection.setName(updatedCollection.getName());
 			existingCollection.setDescription(updatedCollection.getDescription());
 
